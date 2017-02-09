50a31ba26176acc070cfcde0fc02f27c8858f07c
Punit Rathore <punitr@gmail.com>
2017-02-09 12:18:39 -0800

    Creating a dashboard page which renders the dashboard based on the role
    - added an API to fetch the current user
    - created a generic dashboard component

--- src/browser/components/molecules/Layout/index.js
+++ src/browser/components/molecules/Layout/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Layout extends Component {
   render() {
     return <div>
-      <div>This should be the navbar</div>
+      <div></div>
       <div className='layout-content'>
         {this.props.children}
       </div>
--- src/browser/components/pages/MentorDashboard/index.js
+++
@@ -1,2 +0,0 @@
-import React, { Component } from 'react'
-import Layout from '../../molecules/Layout/index'
