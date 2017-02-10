95c1fa6b060211caf5018b2e6e9ccbf56c7d3744
Punit Rathore <punitr@gmail.com>
2017-02-09 18:59:27 -0800

    adding tasks to newbie on signup

--- src/routes/noob.js
+++ src/routes/noob.js
@@ -2,6 +2,7 @@ import express from 'express'
 import path from 'path'
 import * as noob from '../database/queries/noob'
 import * as users from '../database/queries/users'
+import * as tasks from '../database/queries/task'
 
 const router = express.Router()
 
@@ -10,19 +11,24 @@ router.get('/', function(req, res, next){
 })
 
 router.post('/', (request, response, next) => {
+  const currentUser = request.user
   const newNoob = {
-    full_name: request.user.name,
-    github_handle: request.user.handle,
-    email: request.user.email,
+    full_name: currentUser.name,
+    github_handle: currentUser.handle,
+    email: currentUser.email,
     role: request.body.role,
     start_date: request.body.start_date
   }
-  users.findByHandle( request.user.handle ).then( potentialUser => {
+  users.findByHandle( currentUser.handle ).then( potentialUser => {
     if(!potentialUser){
       noob.create( newNoob )
-      .then( results => {
-        response.json({message: 'Welcome to newbie'})
+      .then( noobs => {
+        console.log('noob created:', noobs[0])
+        return tasks.addTemplateTasksByRole('noob', noobs[0].id, noobs[0].start_date)
       })
+      .then(templateTasks => {
+        response.json({message: 'Welcome to newbie'})
+      });
     } else {
       response.json({error: "You are already signed up!"})
     }
