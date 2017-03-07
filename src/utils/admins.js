4d3451c50f5a1524a61fc4c4ceff0755f441d1e7
yaseenagag <yaseen.uc@gmail.com>
2017-03-06 16:55:51 -0800

    tasks are sorted according to title if the days to complete is the same

--- src/utils/admins.js
+++ src/utils/admins.js
@@ -1,4 +1,4 @@
-const ADMIN_GITHUB_HANDLES = ['shereefb', 'carlabagdonas', 'needdra', 'punitrathore']
+const ADMIN_GITHUB_HANDLES = ['shereefb', 'carlabagdonas', 'needdra', 'punitrathore', 'yaseenagag']
 
 export default function isAdmin(githubHandle) {
   return ADMIN_GITHUB_HANDLES.includes(githubHandle)
