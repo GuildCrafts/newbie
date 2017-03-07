71f1900e69de6a08a8c388e5d06d0904d9cb1d85
lizzkats <katsnelson.lizz@gmail.com>
2017-03-07 15:12:57 -0800

    Adds link to file an issue on github in the footer

--- src/browser/components/molecules/Layout/index.js
+++ src/browser/components/molecules/Layout/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import styles from './index.css'
 
 export default class Layout extends Component {
+
   render() {
     return <div className='container'>
       <div></div>
       <div className='layout-content'>
         {this.props.children}
+      </div>
+      <div className={styles.layoutFooter}>
+        <a href='https://github.com/GuildCrafts/newbie/issues'>Add an issue on GitHub</a>
       </div>
     </div>
   }
