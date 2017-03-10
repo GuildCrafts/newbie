e3794e3d1f378bc8ebcac4a9f9f691dbe9d51386
lizzkats <katsnelson.lizz@gmail.com>
2017-03-09 16:04:02 -0800

    styles based on feedback

--- src/browser/components/atoms/Userimage/userImage.js
+++ src/browser/components/atoms/Userimage/userImage.js
@@ -4,7 +4,6 @@ import md5 from 'md5'
 
 export default class UserImage extends Component {
   emailHash(){
-    console.log( '----', this.props )
 
     if(this.props.user !== undefined) {
       return md5(this.props.user.email)
@@ -15,9 +14,7 @@ export default class UserImage extends Component {
 
   render() {
     return (
-      <div>
-        <img className='user-image' src={`https://gravatar.com/avatar/${this.emailHash()}`} />
-      </div>
+      <img className='user-image' src={`https://gravatar.com/avatar/${this.emailHash()}`} />
     )
   }
 }
--- src/browser/components/molecules/Navbar/index.js
+++ src/browser/components/molecules/Navbar/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import styles from './index.css'
- import UserImage from '../../atoms/Userimage/userimage'
+import UserImage from '../../atoms/Userimage/userimage'
 
 export default class Navbar extends Component {
   render() {
